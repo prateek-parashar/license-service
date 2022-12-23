be31d67977f6719d022bcd6b32e61bab33c80eba
prateek-parashar <36658243+prateek-parashar@users.noreply.github.com>
2022-12-24 02:25:29 +0530

    Add controller and service classes to the project

---
+++ src/main/java/com/optimagrowth/license/configuration/MessageConfig.java
@@ -0,0 +1,9 @@
+package com.optimagrowth.license.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MessageConfig {
+
+
+}
--- src/main/java/com/optimagrowth/license/model/License.java
+++ src/main/java/com/optimagrowth/license/model/License.java
@@ -1,4 +1,13 @@
 package com.optimagrowth.license.model;
 
+import lombok.Data;
+
+@Data
 public class License {
+    private int id;
+    private String licenseId;
+    private String description;
+    private String organizationId;
+    private String productName;
+    private String licenseType;
 }
